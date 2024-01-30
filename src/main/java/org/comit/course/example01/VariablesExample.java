0bdda26c40b8e039a0b157b8b1d823c04e81d0f3
joel <okoniha.joel@gmail.com>
2024-01-29 19:55:46 -0600

    first commit

---
+++ src/main/java/org/comit/course/example01/VariablesExample.java
@@ -0,0 +1,24 @@
+package org.comit.course.example01;
+
+public class VariablesExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1 = 4;
+		long num2 = 914748388888L;
+		
+		byte num3 = 5;
+		short num4 = 6;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+
+	}
+
+}
